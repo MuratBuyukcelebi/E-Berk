1e101595d24364bfd59187506a126719db7e1932
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-15 22:25:58 +0300

    Update

---
+++ src/views/Onboarding.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="onboarding">
+    <div class="onboarding__title">Enjoy your online shopping</div>
+    <a href="#">Start shopping</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Onboarding"
+}
+</script>
+
+<style scoped>
+
+</style>
