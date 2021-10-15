977816be067f33f8bcd9cb866dad2a44a9c2122e
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-15 22:25:49 +0300

    Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="">31</div>
+  <div class="launch">
+    <div class="launch__logo"><img src="../assets/icons/logo.png" alt="logo"></div>
+  </div>
 </template>
+
 <script>
 </script>
+
+<style lang="scss">
+.launch {
+  background-image: url("../assets/image/login.svg");
+  height: 100vh;
+  width: 100%;
+  background-color:#000;
+  &__logo {
+    width: 100%;
+    height: 100%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      height: 172px;
+      width: 172px;
+    }
+  }
+}
+</style>
