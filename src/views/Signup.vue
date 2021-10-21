45f468a970558aa6b149efaecb4eefd509e180cd
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-21 23:35:27 +0300

    fix

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,13 +1,22 @@
 <template>
-
+<div class="signup">
+  <div class="container">
+    <SignupContent/>
+  </div>
+</div>
 </template>
 
 <script>
+import SignupContent from "../components/SignupContent";
+
 export default {
-  name: "Signup"
+  name: "Signup",
+  components: {SignupContent}
 }
 </script>
 
 <style lang="scss">
-
+.signup {
+  padding: 68px 0;
+}
 </style>
