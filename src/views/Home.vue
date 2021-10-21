5714d72c0549d585103eb6a4e2442d5ac6682ab6
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-21 21:39:23 +0300

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
   width: 100%;
   background-color:#000;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-position: center;
   &__logo {
     width: 100%;
     height: 100%;
