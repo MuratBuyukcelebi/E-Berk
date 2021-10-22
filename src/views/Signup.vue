60c191a9b9a916ef6848fc664e70aee7608b83d8
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-23 00:06:50 +0300

    update

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,37 +2,37 @@
 <div class="signup">
   <div class="container">
     <SignupContent class="mb-3"/>
-    <div class="signup__divider"> <span>or</span></div>
+    <div class="signup__divider mb-3"><span>or</span></div>
+    <signup-register/>
   </div>
 </div>
 </template>
 
 <script>
 import SignupContent from "../components/SignupContent";
+import SignupRegister from "../components/SignupRegister";
 
 export default {
   name: "Signup",
-  components: {SignupContent}
+  components: {SignupRegister, SignupContent},
 }
 </script>
 
 <style lang="scss">
 .signup {
   padding: 68px 0;
+
   &__divider {
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
 
-
     span {
       color: #9AA7C1;
       font-weight: 500;
       font-size: 11px;
       line-height: 16px;
-      padding: 0 16px;
-      background-color: #FFFFFF;
     }
 
     &:before {
@@ -40,7 +40,17 @@ export default {
       position: absolute;
       height: 1px;
       background-color: #F8F9FC;
-      width: 100%;
+      width: calc(50% - 16px);
+      left: 0;
+      z-index: -1;
+    }
+    &:after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background-color: #F8F9FC;
+      width: calc(50% - 16px);
+      right: 0;
       z-index: -1;
     }
   }
