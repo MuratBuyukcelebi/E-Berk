be06a69c84aed44042ce61695030d039fb160759
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-24 22:37:51 +0300

    add Headline

---
+++ src/components/Headline.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="headline">
+    <div class="d-flex align-items-center mb-2">
+      <a href="#" class="headline__button"><img src="../assets/icons/chevron.svg" alt="icon"></a>
+      <div class="headline__title">
+        <slot name="title"></slot>
+      </div>
+    </div>
+    <div class="headline__text">
+      <slot name="text"></slot>
+    </div>
+    <div class="headline__text">
+      <slot name="text2"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  props: ['title', 'text']
+}
+</script>
+
+<style lang="scss">
+.headline {
+  &__button {
+    height: 24px;
+    width: 24px;
+    margin-right: 12px;
+  }
+  &__title {
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 32px;
+    color: #000;
+  }
+  &__description {
+    max-width: 300px;
+    font-size: 14px;
+    line-height: 24px;
+    color: #5A6987;
+    margin-bottom: 16px;
+  }
+}
+</style>
