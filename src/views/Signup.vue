11921b41fb195da67d551317ea191773bc39a71f
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-10-24 19:52:01 +0300

    fix

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,7 +2,7 @@
 <div class="signup">
   <div class="container">
     <SignupContent class="mb-3"/>
-    <div class="signup__divider mb-3"><span>or</span></div>
+    <Divider/>
     <signup-register/>
       <div class="signup__footer">
         <div class="signup__footer-text">
@@ -27,10 +27,11 @@
 <script>
 import SignupContent from "../components/SignupContent";
 import SignupRegister from "../components/SignupRegister";
+import Divider from "../components/Divider";
 
 export default {
   name: "Signup",
-  components: {SignupRegister, SignupContent},
+  components: {Divider, SignupRegister, SignupContent},
 }
 </script>
 
@@ -38,38 +39,6 @@ export default {
 .signup {
   padding: 68px 0 50px 0;
 
-  &__divider {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    span {
-      color: #9AA7C1;
-      font-weight: 500;
-      font-size: 11px;
-      line-height: 16px;
-    }
-
-    &:before {
-      content: "";
-      position: absolute;
-      height: 1px;
-      background-color: #F8F9FC;
-      width: calc(50% - 16px);
-      left: 0;
-      z-index: -1;
-    }
-    &:after {
-      content: "";
-      position: absolute;
-      height: 1px;
-      background-color: #F8F9FC;
-      width: calc(50% - 16px);
-      right: 0;
-      z-index: -1;
-    }
-  }
   &__footer {
     display: flex;
     justify-content: center;
