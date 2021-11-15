ab917671e78d0107c8cf69738acdb92b8beb16ab
Murat B.Çelebi <73561693+uWozy@users.noreply.github.com>
2021-11-16 01:00:55 +0300

    finished home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -67,29 +67,121 @@
           <a href="#">See All<img src="../assets/icons/right-arrow.svg" alt="icon"></a>
         </div>
         <div class="home-recommended__menu">
-          <div class="home-recommended__menu-item">
-            <div class="home-recommended__menu-item__image">
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
               <img src="../assets/image/home/notebook.png" alt="image">
             </div>
-            <div class="home-recommended__menu-item__title">Macbook Pro 2020</div>
-            <div class="home-recommended__menu-item__description">16-inch, 16GB RAM, 512GB</div>
-            <div class="home-recommended__menu-item__price"><span>$1,499.00</span><span class="discount">$1,640.00</span></div>
+            <div class="home-recommended__item-title">Macbook Pro 2020</div>
+            <div class="home-recommended__item-description">16-inch, 16GB RAM, 512GB</div>
+            <div class="home-recommended__item-price"><span>$1,499.00</span><span class="discount">$1,640.00</span></div>
           </div>
-          <div class="home-recommended__menu-item">
-            <div class="home-recommended__menu-item__image">
-              <img src="../assets/image/home/notebook.png" alt="image">
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/dualshock.png" alt="image">
             </div>
-            <div class="home-recommended__menu-item__title">Macbook Pro 2020</div>
-            <div class="home-recommended__menu-item__description">16-inch, 16GB RAM, 512GB</div>
-            <div class="home-recommended__menu-item__price"><span>$1,499.00</span><span class="discount">$1,640.00</span></div>
+            <div class="home-recommended__item-title">DualShock 4</div>
+            <div class="home-recommended__item-description">Wireless Controller</div>
+            <div class="home-recommended__item-price"><span>$128.00</span><span class="discount">$144.00</span></div>
           </div>
-          <div class="home-recommended__menu-item">
-            <div class="home-recommended__menu-item__image">
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/airdots.png" alt="image">
+            </div>
+            <div class="home-recommended__item-title">Xiaomi Redmi Airdots</div>
+            <div class="home-recommended__item-description">Wireless Headphone</div>
+            <div class="home-recommended__item-price"><span>$25.79</span><span class="discount">$29.00</span></div>
+          </div>
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/ssd.png" alt="image">
+            </div>
+            <div class="home-recommended__item-title">Samsung 860 Evo</div>
+            <div class="home-recommended__item-description">500GB 2.5 Inch Sata III</div>
+            <div class="home-recommended__item-price"><span>$53.99</span><span class="discount">$69.99</span></div>
+          </div>
+        </div>
+      </div>
+      <div class="home-categories">
+        <div class="home-categories__title">
+          <span>Popular categories</span>
+          <a href="#">See All<img src="../assets/icons/right-arrow.svg" alt="icon"></a>
+        </div>
+        <div class="home-categories__list">
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/clothing.png" alt="image"></div>
+            <div class="home-categories__item-title">Clothing</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/seat.png" alt="image"></div>
+            <div class="home-categories__item-title">Furniture</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/bag.png" alt="image"></div>
+            <div class="home-categories__item-title">Shoe & Bag</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/lamb.png" alt="image"></div>
+            <div class="home-categories__item-title">Lighting</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/ps5.png" alt="image"></div>
+            <div class="home-categories__item-title">Console</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/toy.png" alt="image"></div>
+            <div class="home-categories__item-title">Toy</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/book.png" alt="image"></div>
+            <div class="home-categories__item-title">Book</div>
+          </div>
+          <div class="home-categories__item">
+            <div class="home-categories__item-image"><img src="../assets/image/home/category/stationery.png" alt="image"></div>
+            <div class="home-categories__item-title">Stationery</div>
+          </div>
+        </div>
+      </div>
+      <div class="home-banner phone">
+        <div class="home-banner__image"><img src="../assets/image/home/phone.png" alt="image"></div>
+        <div class="home-banner__text">Up to 25% discount on Iphone 11!</div>
+      </div>
+      <div class="home-recommended">
+        <div class="home-recommended__title">
+          <span>Recommended for you</span>
+          <a href="#">See All<img src="../assets/icons/right-arrow.svg" alt="icon"></a>
+        </div>
+        <div class="home-recommended__menu">
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/airdots.png" alt="image">
+            </div>
+            <div class="home-recommended__item-title">Xiaomi Redmi Airdots</div>
+            <div class="home-recommended__item-description">Wireless Headphone</div>
+            <div class="home-recommended__item-price"><span>$25.79</span><span class="discount">$29.00</span></div>
+          </div>
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/ssd.png" alt="image">
+            </div>
+            <div class="home-recommended__item-title">Samsung 860 Evo</div>
+            <div class="home-recommended__item-description">500GB 2.5 Inch Sata III</div>
+            <div class="home-recommended__item-price"><span>$53.99</span><span class="discount">$69.99</span></div>
+          </div>
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
               <img src="../assets/image/home/notebook.png" alt="image">
             </div>
-            <div class="home-recommended__menu-item__title">Macbook Pro 2020</div>
-            <div class="home-recommended__menu-item__description">16-inch, 16GB RAM, 512GB</div>
-            <div class="home-recommended__menu-item__price"><span>$1,499.00</span><span class="discount">$1,640.00</span></div>
+            <div class="home-recommended__item-title">Macbook Pro 2020</div>
+            <div class="home-recommended__item-description">16-inch, 16GB RAM, 512GB</div>
+            <div class="home-recommended__item-price"><span>$1,499.00</span><span class="discount">$1,640.00</span></div>
+          </div>
+          <div class="home-recommended__item">
+            <div class="home-recommended__item-image">
+              <img src="../assets/image/home/dualshock.png" alt="image">
+            </div>
+            <div class="home-recommended__item-title">DualShock 4</div>
+            <div class="home-recommended__item-description">Wireless Controller</div>
+            <div class="home-recommended__item-price"><span>$128.00</span><span class="discount">$144.00</span></div>
           </div>
         </div>
       </div>
@@ -113,7 +205,7 @@
 
     &-header {
       padding-top: 12px;
-      padding-bottom: 32px;
+      padding-bottom: 16px;
 
       &__navigation {
         display: flex;
@@ -170,7 +262,8 @@
       }
     }
     &-special {
-      margin-bottom: 32px;
+      padding-top: 16px;
+      padding-bottom: 16px;
 
       &__title {
         display: flex;
@@ -236,7 +329,8 @@
       background-color: $brand-conch-500;
       border-radius: 6px;
       overflow: hidden;
-      margin-bottom: 32px;
+      margin-top: 16px;
+      margin-bottom: 16px;
 
       &__image {
         position: absolute;
@@ -262,9 +356,20 @@
         top: -110px;
         right: -50px;
       }
+
+      &.phone {
+        background-color: $brand-neutrals-100 !important;
+      }
+      &.phone &__text {
+        color: $brand-neutrals-700 !important;
+      }
+      &.phone &:before {
+        background-image: url("data:image/svg+xml,%3Csvg width='308' height='277' viewBox='0 0 308 277' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='62.2385' height='351.769' transform='matrix(0.632736 0.774367 -0.760867 0.648907 267.649 0)' fill='%23EAEEF5'/%3E%3C/svg%3E%0A") !important;
+      }
     }
     &-recommended {
-      margin-bottom: 32px;
+      padding-top: 16px;
+      padding-bottom: 16px;
 
       &__title {
         display: flex;
@@ -291,57 +396,109 @@
       &__menu {
         white-space: nowrap;
         overflow: auto;
+      }
+      &__item {
+        display: inline-block;
+        margin-right: 16px;
 
-        &-item {
+        &-image {
           display: inline-block;
-          margin-right: 16px;
+          background-color: $brand-neutrals-100;
+          padding: 4px;
+          border-radius: 6px;
+          margin-bottom: 8px;
+        }
+        &-title {
+          font-weight: 500;
+          font-size: 15px;
+          line-height: 24px;
+          letter-spacing: -0.015em;
+          color: #000;
+        }
+        &-description {
+          color: $brand-neutrals-500;
+          font-size: 12px;
+          line-height: 16px;
+          letter-spacing: -0.005em;
+          margin-bottom: 8px;
+        }
+        &-price {
+          display: flex;
 
-          &__image {
-            display: inline-block;
-            background-color: $brand-neutrals-100;
-            padding: 4px;
-            border-radius: 6px;
-            margin-bottom: 8px;
+          & .discount {
+            font-size: 12px;
+            line-height: 16px;
+            letter-spacing: -0.01em;
+            color: $brand-neutrals-300;
+            text-decoration: line-through;
+            display: flex;
+            align-items: flex-start;
           }
-          &__title {
+          span {
             font-weight: 500;
             font-size: 15px;
             line-height: 24px;
             letter-spacing: -0.015em;
-            color: #000;
+            color: $brand-blurple-500;
+            margin-right: 12px;
           }
-          &__description {
-            color: $brand-neutrals-500;
-            font-size: 12px;
-            line-height: 16px;
-            letter-spacing: -0.005em;
-            margin-bottom: 8px;
-          }
-          &__price {
-            display: flex;
+        }
 
-            & .discount {
-              font-size: 12px;
-              line-height: 16px;
-              letter-spacing: -0.01em;
-              color: $brand-neutrals-300;
-              text-decoration: line-through;
-              display: flex;
-              align-items: flex-start;
-            }
-            span {
-              font-weight: 500;
-              font-size: 15px;
-              line-height: 24px;
-              letter-spacing: -0.015em;
-              color: $brand-blurple-500;
-              margin-right: 12px;
-            }
-          }
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+    }
+    &-categories {
+      padding-top: 16px;
+      padding-bottom: 16px;
 
-          &:last-child {
-            margin-right: 0;
-          }
+      &__title {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 16px;
+
+        span {
+          font-weight: bold;
+          font-size: 18px;
+          line-height: 24px;
+          letter-spacing: -0.015em;
+        }
+        a {
+          display: flex;
+          align-items: center;
+          font-weight: 500;
+          font-size: 15px;
+          line-height: 24px;
+          letter-spacing: -0.015em;
+          text-decoration: none;
+          color: $brand-blurple-500;
+        }
+      }
+      &__list {
+        display: grid;
+        grid-template-columns: auto auto auto auto;
+        gap: 16px 18px;
+      }
+      &__item {
+        position: relative;
+        width: 72px;
+
+        &-image {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 72px;
+          width: 72px;
+          margin-bottom: 8px;
+        }
+        &-title {
+          font-weight: 500;
+          font-size: 11px;
+          line-height: 16px;
+          letter-spacing: -0.005em;
+          color: $brand-neutrals-800;
+          text-align: center;
         }
       }
     }
